0401650e265b219a7b1f49cb2ca2d4f1580accf5
MuhammedSmani <muhammedsmani11@gmail.com>
2022-12-20 17:08:22 +0100

    Project Created, Project Folder Structure Organized, Tailwind Installed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./**/*.{html,js}',
+		'./assets/components/**/*.{html,js}',
+		'./assets/pages/**/*.{html,js}',
+		'./assets/scripts/**/*.{html,js}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
