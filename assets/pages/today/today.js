60b90a982197bbeb638be69f34123e362eaf3662
arbrore <arbrore.halilaj@life.gjirafa.com>
2023-01-02 00:19:17 +0100

    Added realtime weather data to the home page

--- assets/pages/today/today.js
+++ assets/pages/today/today.js
@@ -1,13 +1,42 @@
 const form = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
-const weatherCity = document.getElementById('weather-city');
+const realtimeCity = document.getElementById('realtime-city');
+const realtimeTime = document.getElementById('realtime-time');
+const realtimeTemp = document.getElementById('realtime-temp');
+// const dayTemp = document.getElementById('day-temp');
+// const nightTemp = document.getElementById('night-temp');
+const todaysCity = document.getElementById('todays-city');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   const searchKeyword = searchInput.value;
+  
   fetch(`https://api.weatherapi.com/v1/forecast.json?key=9ce000ab2ee94bf8bfd111052222012&q=${searchKeyword}&days=10&aqi=yes&alerts=yes`)
     .then(response => response.json())
     .then(data => {
-      weatherCity.innerHTML = `${data.location.name}, ${data.location.country}`;
+      // Show the city on Realtime Section
+      realtimeCity.innerHTML = `${data.location.name}, ${data.location.country}`;
+
+      // Realtime time near to city
+      const timeString = data.location.localtime;
+      const time = new Date(timeString);
+
+      let hours = time.getHours();
+      let minutes = time.getMinutes();
+      let ampm = 'AM';
+    
+      if (hours > 12) {
+        hours -= 12;
+        ampm = 'PM';
+      }
+
+      realtimeTime.innerHTML = `As of ${hours}:${minutes} ${ampm} CET`;
+
+      // Show the realtime temperature
+      const currentTemp = data.current.temp_c; 
+      realtimeTemp.innerHTML = `${currentTemp}°`;
+
+      // Show the city on Today's Forecast Section
+      todaysCity.innerHTML = `Today's Forecast for ${data.location.name}, ${data.location.country}`;
     });
 });
