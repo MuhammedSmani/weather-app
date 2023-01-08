161e9786ae0f922e2150d6786ddf3b0c0fe438b8
sopjanielona <106884766+sopjanielona@users.noreply.github.com>
2023-01-08 23:18:35 +0100

    Radar Update

--- radar/radar.js
+++ radar/radar.js
@@ -1,12 +1,11 @@
-let map = new ol.Map({
-    target: 'map',  // The ID of the HTML element where the map will be displayed
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()  // Use the OpenStreetMap tiles
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([-79.3832, 43.6532]),  // Toronto, Canada
-      zoom: 8
-    })
-  });
+var map = L.map('map').setView([51.505, -0.09], 7);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('<h1> <b>Mon 02</b><span>|Night</span></h1><h1><b>6°</b></h1><p>Cloudy skies this evening will become partly cloudy after midnight. Areas of patchy fog. Low 4C. Winds light and variable.</p>')
+    .openPopup();
+
