11ee8ead624d60f1f62b2cc18d4fd1807fb576fa
MuhammedSmani <muhammedsmani11@gmail.com>
2023-01-11 15:26:32 +0100

    hourly page accordian

--- assets/pages/hourly/hourly.js
+++ assets/pages/hourly/hourly.js
@@ -1,9 +1,11 @@
-const hourlyArrows = document.querySelectorAll('.hourly-arrow');
-const hourlyHidden = document.querySelectorAll('.hourly__hidden');
+// const hourlyArrows = document.querySelectorAll('.hourly-arrow');
+// const hourlyHidden = document.querySelectorAll('.hourly__hidden');
 
 const hourlyDate = document.querySelector('.hourly__date');
 const hourlyFirstDay = document.querySelector('.hourly__first_day');
+const hourlySecondDay = document.querySelector('.hourly__second_day');
 const hourlyDescription = document.querySelector('.hourly__description');
+const hourlyDescriptionTwo = document.querySelector('.hourly__description_two');
 const hourlyCityName = document.querySelector('.hourly__city_name');
 
 // console.log(hourlyDescription);
@@ -71,6 +73,8 @@ function render() {
 
 			hourlyFirstDay.innerHTML = `${dayName}, ${monthName} ${dateNumber}`;
 
+			hourlySecondDay.innerHTML = `${dayName}, ${monthName} ${dateNumber}`;
+
 			hourlyCityName.innerHTML = `- ${response.location.name}`;
 
 			for (i = 0; i < 24; i++) {
@@ -79,7 +83,7 @@ function render() {
 				const timeString = time.toLocaleString('en-US', options);
 
 				hourlyDescription.innerHTML += `
-          <div class="hourly__main" onclick="openHourly()">
+          <div class="hourly__main">
 									<div>
 										<span>${timeString}</span>
 									</div>
@@ -163,9 +167,111 @@ function render() {
 								</div>
 							</div>
           `;
+				hourlyDescriptionTwo.innerHTML += `
+          <div class="hourly__main">
+									<div>
+										<span>${timeString}</span>
+									</div>
+									<div>
+										<span>${Math.round(response.forecast.forecastday[1].hour[i].temp_c)}°</span>
+									</div>
+									<div><i class="uil uil-cloud"></i> <span>${
+										response.forecast.forecastday[1].hour[i].condition.text
+									}</span></div>
+									<div><i class="uil uil-raindrops"> </i><span>${
+										response.forecast.forecastday[1].hour[i].will_it_rain
+									}%</span></div>
+									<div><i class="uil uil-wind"></i> <span>NW ${
+										response.forecast.forecastday[1].hour[i].wind_kph
+									} kph</span></div>
+									<div class="hourly-arrow">
+										<i class="uil uil-angle-down"></i>
+									</div>
+								</div>
+								<div class="hourly__hidden">
+									<ul class="hourly__hidden__info">
+										<li>
+											<i class="uil uil-thermometer"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__feels__like">Feels like</span>
+												<span class="hourly__feels__like__temp"
+													><b>${Math.round(response.forecast.forecastday[1].hour[i].feelslike_c)}°</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-wind"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__wind">Wind</span>
+												<span class="hourly__wind__temp"
+													><b>NW ${response.forecast.forecastday[1].hour[i].wind_kph} kph</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-tear"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__humidity">Humidity</span>
+												<span class="hourly__humidity__temp"
+													><b>${response.forecast.forecastday[1].hour[i].humidity}%</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-sun"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__uv__index">UV Index</span>
+												<span class="hourly__uv__index__temp"
+													><b>${Math.round(response.forecast.forecastday[1].hour[i].uv)} of 10</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-cloud"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__cloud__cover"
+													>Cloud Cover</span
+												>
+												<span class="hourly__cloud__cover__temp"
+													><b>${response.forecast.forecastday[1].hour[i].cloud}%</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-raindrops-alt"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__rain__amount"
+													>Rain Amount</span
+												>
+												<span class="hourly__rain__amount__temp"
+													><b>${response.forecast.forecastday[1].hour[i].chance_of_rain} mm</b></span
+												>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+          `;
 			}
+			const hourlyArrows = document.querySelectorAll('.hourly-arrow');
+			const hourlyHidden = document.querySelectorAll('.hourly__hidden');
+			// console.log(hourlyArrows);
+
+			hourlyArrows.forEach((arrow, index) => {
+				arrow.addEventListener('click', () => {
+					if (hourlyHidden[index].style.display === 'block') {
+						hourlyHidden[index].style.display = 'none';
+					} else {
+						hourlyHidden.forEach((hidden) => {
+							hidden.style.display = 'none';
+						});
+						hourlyHidden[index].style.display = 'block';
+					}
+				});
+			});
 		});
 }
-function openHourly() {
-	hourlyHidden.style.display == 'hidden';
-}
+
+// function openHourly() {
+// 	hourlyHidden.style.display == 'hidden';
+// }
