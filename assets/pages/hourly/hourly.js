191e424a13f5ebbc41850310c47bcbb15f4068c9
MuhammedSmani <muhammedsmani11@gmail.com>
2023-01-11 19:05:21 +0100

    3 days api added, fixed first day remaining hours showing, and some minor css changes

--- assets/pages/hourly/hourly.js
+++ assets/pages/hourly/hourly.js
@@ -4,8 +4,10 @@
 const hourlyDate = document.querySelector('.hourly__date');
 const hourlyFirstDay = document.querySelector('.hourly__first_day');
 const hourlySecondDay = document.querySelector('.hourly__second_day');
+const hourlyThirdDay = document.querySelector('.hourly__third_day');
 const hourlyDescription = document.querySelector('.hourly__description');
 const hourlyDescriptionTwo = document.querySelector('.hourly__description_two');
+const hourlyDescriptionThree = document.querySelector('.hourly__description_three');
 const hourlyCityName = document.querySelector('.hourly__city_name');
 
 // console.log(hourlyDescription);
@@ -66,18 +68,37 @@ function render() {
 				'Saturday',
 			];
 
-			const date = new Date(`${response.forecast.forecastday[0].date}`);
-			const dayName = days[date.getDay()];
-			const monthName = months[date.getMonth()];
-			const dateNumber = date.getDate();
+			//
 
-			hourlyFirstDay.innerHTML = `${dayName}, ${monthName} ${dateNumber}`;
+			const dateOne = new Date(`${response.forecast.forecastday[0].date}`);
+			const dayNameOne = days[dateOne.getDay()];
+			const monthNameOne = months[dateOne.getMonth()];
+			const dateNumberOne = dateOne.getDate();
 
-			hourlySecondDay.innerHTML = `${dayName}, ${monthName} ${dateNumber}`;
+			hourlyFirstDay.innerHTML = `${dayNameOne}, ${monthNameOne} ${dateNumberOne}`;
+
+			const dateTwo = new Date(`${response.forecast.forecastday[1].date}`);
+			const dayNameTwo = days[dateTwo.getDay()];
+			const monthNameTwo = months[dateTwo.getMonth()];
+			const dateNumberTwo = dateTwo.getDate();
+
+			hourlySecondDay.innerHTML = `${dayNameTwo}, ${monthNameTwo} ${dateNumberTwo}`;
+
+			const dateThree = new Date(`${response.forecast.forecastday[2].date}`);
+			const dayNameThree = days[dateThree.getDay()];
+			const monthNameThree = months[dateThree.getMonth()];
+			const dateNumberThree = dateThree.getDate();
+
+			hourlyThirdDay.innerHTML = `${dayNameThree}, ${monthNameThree} ${dateNumberThree}`;
+
+			//
 
 			hourlyCityName.innerHTML = `- ${response.location.name}`;
 
-			for (i = 0; i < 24; i++) {
+			let currentHour = new Date().getHours();
+			// console.log(currentHour);
+
+			for (i = currentHour; i < 24; i++) {
 				const time = new Date(`${response.forecast.forecastday[0].hour[i].time}`);
 				const options = { hour: 'numeric', hour12: true };
 				const timeString = time.toLocaleString('en-US', options);
@@ -167,6 +188,12 @@ function render() {
 								</div>
 							</div>
           `;
+			}
+
+			for (i = 0; i < 24; i++) {
+				const time = new Date(`${response.forecast.forecastday[0].hour[i].time}`);
+				const options = { hour: 'numeric', hour12: true };
+				const timeString = time.toLocaleString('en-US', options);
 				hourlyDescriptionTwo.innerHTML += `
           <div class="hourly__main">
 									<div>
@@ -245,6 +272,91 @@ function render() {
 												>
 												<span class="hourly__rain__amount__temp"
 													><b>${response.forecast.forecastday[1].hour[i].chance_of_rain} mm</b></span
+												>
+											</div>
+										</li>
+									</ul>
+								</div>
+							</div>
+          `;
+				hourlyDescriptionThree.innerHTML += `
+          <div class="hourly__main">
+									<div>
+										<span>${timeString}</span>
+									</div>
+									<div>
+										<span>${Math.round(response.forecast.forecastday[2].hour[i].temp_c)}°</span>
+									</div>
+									<div><i class="uil uil-cloud"></i> <span>${
+										response.forecast.forecastday[2].hour[i].condition.text
+									}</span></div>
+									<div><i class="uil uil-raindrops"> </i><span>${
+										response.forecast.forecastday[2].hour[i].will_it_rain
+									}%</span></div>
+									<div><i class="uil uil-wind"></i> <span>NW ${
+										response.forecast.forecastday[2].hour[i].wind_kph
+									} kph</span></div>
+									<div class="hourly-arrow">
+										<i class="uil uil-angle-down"></i>
+									</div>
+								</div>
+								<div class="hourly__hidden">
+									<ul class="hourly__hidden__info">
+										<li>
+											<i class="uil uil-thermometer"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__feels__like">Feels like</span>
+												<span class="hourly__feels__like__temp"
+													><b>${Math.round(response.forecast.forecastday[2].hour[i].feelslike_c)}°</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-wind"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__wind">Wind</span>
+												<span class="hourly__wind__temp"
+													><b>NW ${response.forecast.forecastday[2].hour[i].wind_kph} kph</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-tear"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__humidity">Humidity</span>
+												<span class="hourly__humidity__temp"
+													><b>${response.forecast.forecastday[2].hour[i].humidity}%</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-sun"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__uv__index">UV Index</span>
+												<span class="hourly__uv__index__temp"
+													><b>${Math.round(response.forecast.forecastday[2].hour[i].uv)} of 10</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-cloud"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__cloud__cover"
+													>Cloud Cover</span
+												>
+												<span class="hourly__cloud__cover__temp"
+													><b>${response.forecast.forecastday[2].hour[i].cloud}%</b></span
+												>
+											</div>
+										</li>
+										<li>
+											<i class="uil uil-raindrops-alt"></i>
+											<div class="hourly__hidden__info__text">
+												<span class="hourly__rain__amount"
+													>Rain Amount</span
+												>
+												<span class="hourly__rain__amount__temp"
+													><b>${response.forecast.forecastday[2].hour[i].chance_of_rain} mm</b></span
 												>
 											</div>
 										</li>
