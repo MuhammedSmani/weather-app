9b6bf03d136048f9d30dc35311f0c86576f8d56d
sopjanielona <106884766+sopjanielona@users.noreply.github.com>
2023-01-16 00:30:27 +0100

    Sevenday update
    Math round temperatures
    Arrow fix
    Default data with geolocation
    CSS fixes

--- radar/radar.js
+++ radar/radar.js
@@ -31,9 +31,9 @@ if ("geolocation" in navigator) {
                 let output = "";
                 output += `
               <h2>${data.location.name}</h2>
-              <h1><b>${data.current.temp_c}°</b></h1>
+              <h1><b>${Math.round(data.current.temp_c)}°</b></h1>
               <h2>${data.current.condition.text}</h2>
-              <h2>Feels like ${data.current.feelslike_c}°</h2>
+              <h2>Feels like ${Math.round(data.current.feelslike_c)}°</h2>
               <img src="${data.current.condition.icon}">
 
 
@@ -88,9 +88,9 @@ function showDataOnMap(data) {
     let output = "";
     output += `
         <h2>${data.location.name}</h2>
-        <h1><b>${data.current.temp_c}°</b>
+        <h1><b>${Math.round(data.current.temp_c)}°</b>
         <h2>${data.current.condition.text}</h2>
-        <h2>Feels like ${data.current.feelslike_c}°</h2>
+        <h2>Feels like ${Math.round(data.current.feelslike_c)}°</h2>
          <img src="${data.current.condition.icon}">
     `;
     customIcon = L.icon({
@@ -140,7 +140,7 @@ function getMapIcons() {
                     });
 
                     var marker = L.marker([lat, lng], { icon: customIcon }).addTo(map);
-                    marker.bindPopup(`<h2>${city}<span><b>  ${data.current.temp_c}°</b></span></h2>`);
+                    marker.bindPopup(`<h2>${city}<span><b>  ${Math.round(data.current.temp_c)}°</b></span></h2>`);
                     map.setView([48.15, 17.02], 4);
                     markers.push(marker);
 
@@ -191,7 +191,7 @@ function getHighTemp(data) {
     });
 
     var marker = L.marker([lat, lng], { icon: highTempIcon }).addTo(map);
-    marker.bindPopup(`<h2>${data.location.name}<span><b>  ${data.current.temp_c}°</b></span></h2>`);
+    marker.bindPopup(`<h2>${data.location.name}<span><b>  ${Math.round(data.current.temp_c)}°</b></span></h2>`);
                 markers.push(marker);
 
 }
@@ -206,7 +206,7 @@ function getMediumTemp(data) {
     });
 
     var marker = L.marker([lat, lng], { icon: mediumTempIcon }).addTo(map);
-    marker.bindPopup(`<h2>${data.location.name}<span><b>  ${data.current.temp_c}°</b></span></h2>`);
+    marker.bindPopup(`<h2>${data.location.name}<span><b>  ${Math.round(data.current.temp_c)}°</b></span></h2>`);
     markers.push(marker);
 }
 
@@ -219,5 +219,5 @@ function getLowTemp(data) {
         iconSize: [25, 25]
     });
     var marker = L.marker([lat, lng], { icon: lowTempIcon }).addTo(map);
-    marker.bindPopup(`<h2>${data.location.name}<span><b>  ${data.current.temp_c}°</b></span></h2>`);
+    marker.bindPopup(`<h2>${data.location.name}<span><b>  ${Math.round(data.current.temp_c)}°</b></span></h2>`);
     markers.push(marker);}                
--- sevenday/sevenday.js
+++ sevenday/sevenday.js
@@ -1,22 +1,26 @@
 // const weeklyDescription = document.getElementById('weekly-main');
 const weeklyDescription = document.querySelector('.weekly__description');
 
+const weeklyTitle = document.querySelector('.weekly__title');
+
 const form = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 
+ weeklyDescription.innerHTML = '';
 
 
-function getMainData(data) {
+
+function showData(data) {
   data.forecast.forecastday.forEach((day, index) => {
     weeklyDescription.innerHTML += `  <div class="weekly__main" id="weekly-${index}" >
     <div>
-        <span>Mon 2nd</span>
+        <span>${day.date}</span>
     </div>
     <div>
-        <span><b>6°</b></span><span>/3°</span>
+        <span><b>${Math.round(day.day.maxtemp_c)}°</b></span><span>/${Math.round(day.day.mintemp_c)}°</span>
     </div>
     <div>
-        <i class="uil uil-cloud"></i> <span>Light Rain</span>
+        <i class="uil uil-cloud"></i> <span>${day.day.condition.text}</span>
     </div>
     <div>
         <i class="uil uil-raindrops"> </i><span>64%</span>
@@ -29,9 +33,10 @@ function getMainData(data) {
   </div>
   </div>
   <div class="weekly__hidden" id="hidden-${index}">
+  <div class="day-hidden"> 
   <div>
-    <h1> <b>${day.date}</b><span>|Night</span></h1>
-    <h1><b>${day.day.maxtemp_c}°</b></h1>
+    <h1> <b>${day.date}</b><span>|Day</span></h1>
+    <h1><b>${Math.round(day.day.maxtemp_c)}°</b></h1>
     <p>${day.day.condition.text}</p>
     <div>
         <i class=" uil uil-raindrops"></i> <span>${day.day.daily_chance_of_rain}%</span>
@@ -84,40 +89,135 @@ function getMainData(data) {
         </div>
     </li>
   </ul>
+  </div>
+  <div class="night-hidden"> 
+  <div>
+    <h1> <b>${day.date}</b><span>|Night</span></h1>
+    <h1><b>${Math.round(day.day.maxtemp_c)}°</b></h1>
+    <p>${day.day.condition.text}</p>
+    <div>
+        <i class=" uil uil-raindrops"></i> <span>${day.day.daily_chance_of_rain}%</span>
+    </div>
+    <div>
+        <i class="uil uil-wind"></i> <span>NW ${day.day.maxwind_kph} km/h</span>
+    </div>
+  </div>
+  <ul class="weekly__hidden__info">
+    <li>
+        <i class="uil uil-thermometer"></i>
+        <div class="weekly__hidden__info__text">
+            <span class="weekly__feels__like">Feels like</span>
+            <span class="weekly__feels__like__temp"><b>${day.day.feelslike_c}°</b></span>
+        </div>
+    </li>
+    <li>
+        <i class="uil uil-wind"></i>
+        <div class="weekly__hidden__info__text">
+            <span class="weekly__wind">Wind</span>
+            <span class="weekly__wind__temp"><b>NW ${day.day.maxwind_kph} km/h</b></span>
+        </div>
+    </li>
+    <li>
+        <i class="uil uil-tear"></i>
+        <div class="weekly__hidden__info__text">
+            <span class="weekly__humidity">Humidity</span>
+            <span class="weekly__humidity__temp"><b>${day.day.avghumidity}%</b></span>
+        </div>
+    </li>
+    <li>
+        <i class="uil uil-sun"></i>
+        <div class="weekly__hidden__info__text">
+            <span class="weekly__uv__index">UV Index</span>
+            <span class="weekly__uv__index__temp"><b>${day.day.uv} of 10</b></span>
+        </div>
+    </li>
+    <li>
+        <i class="uil uil-cloud"></i>
+        <div class="weekly__hidden__info__text">
+            <span class="weekly__cloud__cover">Cloud Cover</span>
+            <span class="weekly__cloud__cover__temp"><b>${day.day.cloud}%</b></span>
+        </div>
+    </li>
+    <li>
+        <i class="uil uil-raindrops-alt"></i>
+        <div class="weekly__hidden__info__text">
+            <span class="weekly__rain__amount">Rain Amount</span>
+            <span class="weekly__rain__amount__temp"><b>${day.day.totalprecip_mm} mm</b></span>
+        </div>
+    </li>
+  </ul>
+  </div>
   </div>`;
-})
-const weeklyArrows = document.querySelectorAll('.weekly-arrow');
-const weeklyHidden = document.querySelectorAll('.weekly__hidden');
+  })
+  const weeklyArrows = document.querySelectorAll('.weekly-arrow');
+  const weeklyHidden = document.querySelectorAll('.weekly__hidden');
 
-    
-weeklyArrows.forEach((arrow, index) => {
+
+  weeklyArrows.forEach((arrow, index) => {
     arrow.addEventListener('click', () => {
-        if (weeklyHidden[index].style.display === 'block') {
-            weeklyHidden[index].style.display = 'none';
-        } else {
-            weeklyHidden.forEach((hidden) => {
-                hidden.style.display = 'none';
+      if (weeklyHidden[index].style.display === 'block') {
+        weeklyHidden[index].style.display = 'none';
+      } else {
+        weeklyHidden.forEach((hidden) => {
+          hidden.style.display = 'none';
 
-            });
-            weeklyHidden[index].style.display = 'block';
+        });
+        weeklyHidden[index].style.display = 'block';
 
-        }
+      }
     });
-})
+  })
 
-  ;
+
+
+    ;
 }
 
 if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        var lat = position.coords.latitude,
-            lng = position.coords.longitude;
-        fetch(
-            `https://api.weatherapi.com/v1/forecast.json?key=9ce000ab2ee94bf8bfd111052222012&q=${lat},${lng}&days=10&aqi=yes&alerts=yes`
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                getMainData(data);})})};
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var lat = position.coords.latitude,
+      lng = position.coords.longitude;
+    fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=9ce000ab2ee94bf8bfd111052222012&q=${lat},${lng}&days=10&aqi=yes&alerts=yes`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        showTitleData(data);
+        showData(data);
+      })
+  })
+};
 
 
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const searchKeyword = searchInput.value;
+
+  getMainData(searchKeyword);
+});
+
+function getMainData(lokacioni) {
+  weeklyDescription.innerHTML = '';
+  weeklyTitle.innerHTML = '';
+
+
+  fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=9ce000ab2ee94bf8bfd111052222012&q=${lokacioni}&days=10&aqi=yes&alerts=yes`
+  )
+      .then((response) => response.json())
+      .then((data) => {
+        showTitleData(data);
+
+          showData(data);
+      });
+}
+
+function showTitleData(data){
+  weeklyTitle.innerHTML += `
+  <h1>7 Day Weather - <span id="city">${data.location.name}</span></h1>
+  <p>As of <span id="current-time">3:44 pm</span>  CET</p>
+  `
+
+}
 
